0c5a6641e36ad8ed8553334bbecf626cd994437b
JonathanF1234 <156109904+JonathanF1234@users.noreply.github.com>
2024-01-18 10:23:47 -0600

    first map and basic movement

--- main.ts
+++ main.ts
@@ -1,1 +1,37 @@
-
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (jump < 2) {
+        jump += 1
+        mySprite.vy = -150
+    }
+})
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (!(mySprite.isHittingTile(CollisionDirection.Top))) {
+        jump = 0
+    }
+})
+let jump = 0
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level`)
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(30, 120)
+controller.moveSprite(mySprite, 100, 0)
+scene.cameraFollowSprite(mySprite)
+mySprite.ay = 300
+jump = 0
